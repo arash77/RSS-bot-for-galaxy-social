8c9528b3b71277e1c84e75f55ce188d5051b1511
Arash <arash77.kad@gmail.com>
2024-05-28 12:58:06 +0200

    Refactor feed_bot.py for improved error handling and code organization

--- app/feed_bot.py
+++ app/feed_bot.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import datetime
 
 import feedparser
@@ -13,6 +14,7 @@ class feed_bot:
         try:
             with open(feed_file, "r") as file:
                 self.configs = yaml.safe_load(file)
+
         except FileNotFoundError:
             raise FileNotFoundError(f"File {feed_file} not found")
 
@@ -38,34 +40,61 @@ class feed_bot:
         )
 
         feed_list = self.configs.get("feeds")
+        if feed_list is None:
+            raise ValueError("No feeds found in the file")
         for feed in feed_list:
+            if feed.get("url") is None:
+                raise ValueError(f"No url found in the file for feed {feed}")
+            elif feed.get("media") is None:
+                raise ValueError(f"No media found in the file for feed {feed}")
+            elif feed.get("format") is None:
+                raise ValueError(f"No format found in the file for feed {feed}")
             try:
-                feed_data = feedparser.parse(feed)
+                feed_data = feedparser.parse(feed.get("url"))
             except Exception as e:
-                print(f"Error in parsing feed {feed}: {e}")
+                print(f"Error in parsing feed {feed.get('url')}: {e}")
                 continue
             folder = feed_data.feed.title.replace(" ", "_").lower()
             for entry in feed_data.entries:
+                if file_path in self.existing_files:
+                    print(f"File {file_path} already exists")
+                    continue
+
+                if entry.link is None:
+                    print(f"No link found for entry {entry.title}")
+                    continue
+
                 sub_folder = entry.link.split("/")[-1] or entry.link.split("/")[-2]
                 file_path = f"{self.feed_bot_path}/{folder}/{sub_folder}.md"
 
-                if file_path in self.existing_files:
-                    continue
-
-                medias = "\n".join(
-                    [f"  - {media}" for media in self.configs.get("media")]
+                md_config = yaml.dump(
+                    {
+                        key: feed[key]
+                        for key in ["media", "mentions", "hashtags"]
+                        if key in feed
+                    }
                 )
 
-                summary = BeautifulSoup(entry.summary, "html.parser")
-                if entry.summary == entry.content[0].value:
-                    print(f"Entry {entry.title} has no summary")
-                    try:
-                        summary = summary.find("p")
-                    except:
-                        summary = ""
+                format_string = feed.get("format")
+                placeholders = re.findall(r"{(.*?)}", format_string)
+                values = {}
+                for placeholder in placeholders:
+                    if placeholder in entry:
+                        if "<p>" in entry[placeholder]:
+                            soup = BeautifulSoup(entry[placeholder], "html.parser")
+                            first_paragraph = soup.find("p")
+                            values[placeholder] = first_paragraph.get_text().replace(
+                                "\n", " "
+                            )
+                        else:
+                            values[placeholder] = entry[placeholder]
+                    else:
+                        print(
+                            f"Placeholder {placeholder} not found in entry {entry.title}"
+                        )
+                formatted_text = format_string.format(**values)
 
-                summary = summary.get_text(strip=True).replace("\n", " ")
-                md_content = f"---\nmedia:\n{medias}\n---\n{entry.title}\n{summary}\n\nRead more: {entry.link}\n"
+                md_content = f"---\n{md_config}---\n{formatted_text}"
 
                 self.repo.create_file(
                     path=file_path,
